afe7d20423bd39a01cfc3677a3bc8089ae097885
Reid Kleckner <rnk@google.com>
2016-08-02 18:21:04 -0700

    wip

--- test/asan/TestCases/ill.cc
+++ test/asan/TestCases/ill.cc
@@ -1,8 +1,10 @@
-// RUN: %clangxx_asan -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
-// RUN: %clangxx_asan -O2 %s -o %t && not %run %t 2>&1 | FileCheck %s
 // Test the handle_sigill option.
+//
+// RUN: %clangxx_asan %s -o %t && %env_asan_opts=handle_sigill=0 not --crash %run %t 2>&1 | FileCheck %s --check-prefix=CHECK0
+// RUN: %clangxx_asan %s -o %t && %env_asan_opts=handle_sigill=1 not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK1
 
 int main() {
   __builtin_trap();
 }
-// CHECK: ERROR: AddressSanitizer: {{ILL|illegal-instruction}} on unknown address {{0x0*}}
+// CHECK0-NOT: ERROR: AddressSanitizer
+// CHECK1: ERROR: AddressSanitizer: {{ILL|illegal-instruction}} on unknown address {{0x0*}}
